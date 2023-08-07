a899f4ce23b7d75d726edb83bf08d14407ccab03
Renny Moreira <138821161+RennyMoreira@users.noreply.github.com>
2023-08-06 23:09:35 -0500

    Agregar archivos de proyecto.

---
+++ Uso PIlas Colas/ColaDecimal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uso_PIlas_Colas
+{
+    public class ColaDecimal //Clase
+    {
+        private List<decimal> datos;
+
+        public ColaDecimal()
+        {
+            datos = new List<decimal>();
+        }
+
+        public int Cantidad
+        {
+            get { return datos.Count; }
+
+        }
+
+        public decimal Encolar(decimal n)
+        {
+            datos.Add(n);
+            return n;
+        }
+
+        public decimal Descencolar()
+        {
+            decimal dato = datos[0];
+            datos.RemoveAt(0);
+            return dato;
+        }
+    }
+}
---
+++ Uso PIlas Colas/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ColaDecimal //Clase ColaDecimal
+{
+    private List<decimal> datos;
+
+    public ColaDecimal()
+    {
+        datos = new List<decimal>();
+    }
+
+    public int Cantidad//Devuelve la cantidad de datos
+    {
+        get { return datos.Count; }
+        
+    }
+
+    public decimal Encolar(decimal n)//Método Encolar(Enqueue) ingresa datos al final de la lista  
+    {
+        datos.Add(n);
+        return n;
+    }
+
+    public decimal Descencolar()//Método Descencolar(Dequeue) al dato lo saca de la lista  
+    {
+        decimal dato = datos [0];
+        datos.RemoveAt(0);
+        return dato;
+    }
+    
+}
+
+class Cola //Clase Cola
+{
+    static void Main(string[] args)
+    {
+        ColaDecimal cola = new ColaDecimal();
+        
+        string opcion;
+        do
+        {//Menú de opciones para el ingreso de los datos
+            Console.WriteLine("1.- Añadir un dato");
+            Console.WriteLine("2.- Imprimir los datos");
+            Console.WriteLine("0.- Ingrese 0 para salir");
+            opcion = Console.ReadLine();
+            switch (opcion)
+            {
+                case "1":
+                    Console.WriteLine("Ingrese un número: ");
+                    cola.Encolar(Convert.ToDecimal(
+                        Console.ReadLine()));
+                    break;
+                case "2":
+                    while (cola.Cantidad > 0)
+                    {
+                        Console.WriteLine(cola.Descencolar);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        while (opcion != "0");
+
+    }
+}
